e341b3a70082cfe51cf08c7842f3a05850d2817b
brianyu18 <brianyu1990@gmail.com>
2018-09-27 04:46:48 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,13 +3,12 @@ def new_hash
 end
 
 def actor
-  actor = {name: 'Dwayne The Rock Johnson'} 
+  actor = {:name => 'Dwayne The Rock Johnson'} 
 end
 
 def monopoly
 	monopoly = {}
-  road = monopoly[:railroads] = {}
-  road
+  monopoly[:railroads] = {}
 end
 
 def monopoly_with_second_tier
