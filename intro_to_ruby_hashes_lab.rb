2b3d079bb1f2028b1850fc28c1615b58e9ea349c
brianyu18 <brianyu1990@gmail.com>
2018-09-27 04:45:37 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,27 +1,47 @@
 def new_hash
-
+  hash = {}
 end
 
 def actor
-
+  actor = {name: 'Dwayne The Rock Johnson'} 
 end
 
 def monopoly
 	monopoly = {}
-
+  monopoly[:railroads] = {}
 end
 
 def monopoly_with_second_tier
+  monopoly = {}
+  monopoly[:railroads] = {}
+  monopoly[:railroads][:pieces] = 4
+  monopoly[:railroads][:name] = {}
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 	
 end
 
 def monopoly_with_third_tier
+  monopoly = {}
+  monopoly[:railroads][:rent_in_dollars] = {}
+  monopoly[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
+  monopoly[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
+  monopoly[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
+  monopoly[:railroads][:name][:reading_railroad] = {}
+  monopoly[:railroads][:name][:pennsylvania_railroad] = {}
+  monopoly[:railroads][:name][:b_and_o_railroad] = {}
+  monopoly[:railroads][:name][:shortline_railroad] = {}
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 		
 end
 
 def monopoly_with_fourth_tier
+  monopoly = {}
+  monopoly[:railroads] = {}
+  monopoly[:railroads][:rent_in_dollars][:one_piece_owned] = 25
+  monopoly[:railroads][:name][:reading_railroad]['mortgage_value'] = '$100'
+  monopoly[:railroads][:name][:pennsylvania_railroad]['mortgage_value'] = '$200'
+  monopoly[:railroads][:name][:b_and_o_railroad]['mortgage_value'] = '$400'
+  monopoly[:railroads][:name][:shortline_railroad]['mortgage_value'] = '$800'
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
 	
 end
