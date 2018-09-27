3f4759a81174694c1930c3257459a6e8fc84a16a
brianyu18 <brianyu1990@gmail.com>
2018-09-27 05:42:36 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,7 +36,7 @@ def monopoly_with_third_tier
   monopoly[:railroads][:names][:reading_railroad] = {}
   monopoly[:railroads][:names][:pennsylvania_railroad] = {}
   monopoly[:railroads][:names][:b_and_o_railroad] = {}
-  monopoly[:railroads][:names][:shortline_railroad] = {}
+  monopoly[:railroads][:names][:shortline] = {}
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 	monopoly	
 end
